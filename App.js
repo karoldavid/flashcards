8f2273394be967069f336fc5c700057559066747
karoldavid <k.zysk@zoho.com>
2017-10-09 18:54:09 +0200

    feat: add redux store

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
 import DeckListView from './components/DeckListView'
 
 export default class App extends React.Component {
   render() {
     return (
-      <DeckListView />
+    	<Provider store={createStore(reducers)}>
+      		<DeckListView />
+      	</Provider>
     )
   }
 }
