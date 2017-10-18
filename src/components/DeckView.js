e1dd37b7385c4a995d68c2b59bb82b12b7465ac4
karoldavid <k.zysk@zoho.com>
2017-10-18 21:27:56 +0200

    feat: add styles to deck view

--- src/components/DeckView.js
+++ src/components/DeckView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import {View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { lightPurp, white } from '../utils/colors'
 
 class DeckView extends Component {
 	render() {
@@ -8,23 +9,46 @@ class DeckView extends Component {
 		const { questions } = this.props.currentDeck
 
 		return (
-			<View>
-				<Text style={styles.textStyles}>{params.title}</Text>
-				<Text>{`${questions.length} card${questions.length > 1 ? 's' : ''}`}</Text>
+			<View style={styles.container}>
+				<Text style={styles.deckTitle}>{params.title}</Text>
+				<Text style={styles.deckContent}>{`${questions.length} card${questions.length > 1 ? 's' : ''}`}</Text>
 			</View>
 		)
 	}
 }
 
-const styles = {
-	textStyles: {
-		fontSize: 18
-	}
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: lightPurp,
+    margin: 5
+  },
+  deckTitle: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 16,
+    fontSize: 18,
+    color: white
+  },
+  deckContent: {
+  	flex: 1,
+    flexDirection: 'row',
+  	fontSize: 16,
+  	color: white
+  },
+  deckAction: {
+    margin: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+  }
+});
 
 const mapStateToProps = (state) => {
+	const { flashCards, selectDeck } = state
     return {
-        currentDeck: state.flashCards[state.selectDeck]
+        currentDeck: flashCards[selectDeck]
     }
 }
 
