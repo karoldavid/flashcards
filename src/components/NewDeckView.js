1ff489a2d242e86b264e59b8d6772462f91e6919
karoldavid <k.zysk@zoho.com>
2017-10-20 12:23:09 +0200

    feat: add text input for new deck title to new deck view

--- src/components/NewDeckView.js
+++ src/components/NewDeckView.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { Text, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native'
+import { lightPurp, lightBrilliantBlueMagenta, white } from '../utils/colors'
 
 class NewDeckView extends Component {
 
+	state = {text: ''}
+
 	render() {
-		const { params } = this.props.navigation.state;
+		const { params } = this.props.navigation.state
+		const { containerStyles, titleStyles, inputStyles } = styles 
 
 		return(
-			<View>
-				<Text style={{ fontSize: 18 }}>
-					{params.title}
+			<KeyboardAvoidingView behavior='padding' style={containerStyles}>
+				<Text style={titleStyles}>
+					What is the title of your new deck?
 				</Text>
-			</View>
+				<TextInput
+			        style={inputStyles}
+			        placeholder="type title here"
+			        onChangeText={(text) => this.setState({text})}
+			    />
+			</KeyboardAvoidingView>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	containerStyles: {
+    	flex: 1,
+    	alignItems: 'center',
+    	justifyContent: 'center',
+    	backgroundColor: lightPurp,
+    	margin: 5
+    },
+    titleStyles: {
+    	fontSize: 18,
+    	color: white
+    },
+    inputStyles: {
+      margin: 15,
+      height: 40,
+      width: 200,
+      borderColor: lightBrilliantBlueMagenta,
+      borderWidth: 1,
+      color: white
+   },
+})
 
 export default NewDeckView
